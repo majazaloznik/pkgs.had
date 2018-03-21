f9bb14080514cfc82544aa5f9624ef459c3a07bd
majazaloznik <maja.zaloznik@gmail.com>
2018-03-21 19:36:26 +0000

    chapters 2 and 3 or so

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,37 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
+#' this is a test
+#'
+#' @param d is a parameter
+#' @examples
+#' hello()
+#'some mroe stuff
+#'
+#'
+#'dsd person("Hadley", "Wickham", email = "hadley@rstudio.com", role = c("aut", "cre"))  person("Hadley", "Wickham", email = "hadley@rstudio.com", role = c("aut", "cre"))
+#'
+#'
+hello <- function(d) {
   print("Hello, world!")
 }
+
+
+
+
+#' Title
+#'
+#' @param s
+#' @param f
+#'
+#' @return
+#' @export
+#'
+#' @examples
+new <- function(s,f) {
+  #do sth
+}
+
+
+
+
